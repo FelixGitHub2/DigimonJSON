6c18ab33a664a401c283f8edde04b3b34a3396db
Felix Elliot <felix.elliot@elev.ga.ntig.se>
2024-01-25 14:34:08 +0100

    first commit

---
+++ DigimonJSON/Digimon.cs
@@ -0,0 +1,15 @@
+using System.Dynamic;
+using RestSharp;
+using System.Text.Json;
+
+namespace DigiminJSON
+{
+    public class Digimon
+    {
+        public string name { get; set; }
+
+        
+    }
+
+}
+
---
+++ DigimonJSON/Program.cs
@@ -0,0 +1,14 @@
+﻿using DigiminJSON;
+using RestSharp;
+using System.Text.Json;
+
+RestClient client = new("https://digi-api.com/api/v1");
+
+RestRequest request = new($"digimon/38");
+
+RestResponse response = client.GetAsync(request).Result;
+
+Digimon d = JsonSerializer.Deserialize<Digimon>(response.Content);
+
+Console.WriteLine(d.name);
+Console.ReadLine();
